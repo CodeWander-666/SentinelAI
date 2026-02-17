3fbda2a9d96cd07f2267e8f5b22a4517ade3d301
CodeWander-666 <nikhilsinghp916@gmail.com>
2026-02-17 20:53:04 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -6,5 +6,5 @@ class PipelineTracker:
         self.box = box
         self.bar = bar
     def log(self, msg, pct):
-        self.box.code(f"[{time.strftime('%H:%M:%S')}] {msg}", language="bash")
-        self.bar.progress(pct, text=msg)
+        self.box.caption(f"[{time.strftime('%H:%M:%S')}] {msg}")
+        self.bar.progress(pct)
